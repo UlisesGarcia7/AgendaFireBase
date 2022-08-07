0bb4d6abcfdb03fb539627945492bb17e1a05c95
UlisesGarcia7 <2019030286@upsin.edu.mx>
2022-08-06 21:26:34 -0600

    Agregado

---
+++ app/src/main/java/com/example/agendafirebase/Objetos/ReferenciasFireBase.java
@@ -0,0 +1,7 @@
+package com.example.agendafirebase.Objetos;
+
+public class ReferenciasFireBase {
+    final static public String URL_DATABASE = "https://appagenda-a9606-default-rtdb.firebaseio.com/";
+    final static public String DATABASE_NAME = "Agenda";
+    final static public String TABLE_NAME = "Contactos";
+}
